fbce85fe526765227c8bed0aedcaac26886e0115
Leonard Zhang <54585080+leleonardzhang@users.noreply.github.com>
2020-10-17 22:09:28 -0400

    Add files via upload

--- flask-server/transfer.py
+++ flask-server/transfer.py
@@ -3,8 +3,12 @@ from flask import Flask, flash, request, redirect, url_for
 from werkzeug.utils import secure_filename
 import datetime
 
+timing = 0
+
 time_stamp = datetime.datetime.now().strftime("%m%d%Y%H%M%S")
-UPLOAD_DIRECTORY = f'./{time_stamp}' 
+UPLOAD_DIRECTORY = f'./{time_stamp}'
+
+f = open("./log")
 
 if not os.path.exists(UPLOAD_DIRECTORY):
     os.makedirs(UPLOAD_DIRECTORY)
@@ -12,21 +16,31 @@ if not os.path.exists(UPLOAD_DIRECTORY):
 if not os.path.exists("./result"):
     os.makedirs("./result")
 
+if not os.path.exists("./temp"):
+    os.makedirs("./temp")
+
+os.system("rm -rf ./temp/*")
+
 ALLOWED_EXTENSIONS = {'mid'}
 api = Flask(__name__)
 
 # Upload file with name = "example-name"
 @api.route("/post-file", methods = ['POST'])
 def post_file():
+    global timing
+    # post file
     file = request.files['example-name']    # should be same as name field in <input>
     filename = secure_filename(file.filename)
     if file:
         file.save(os.path.join(UPLOAD_DIRECTORY, filename))
+    # post time
+    timing = request.form['time']           # should be same as name field in <input>
     return #return webpage or something here after upload
 
 @api.route("/begin-transfer", methods = ['GET','POST'])
 def begin_transfer():
-    os.system(f'python3 ./lstm.py {UPLOAD_DIRECTORY}')
+    os.system(f'python3 ./lstm.py {UPLOAD_DIRECTORY} {timing}')
+    os.system(f'python3 ./predict.py')
     return
 
 # Download file
