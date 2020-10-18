a1fa7371840e4f628660f3afdfaadb72e1b588ba
Leonard Zhang <54585080+leleonardzhang@users.noreply.github.com>
2020-10-17 22:03:27 -0400

    Add files via upload

--- transfer.py
+++ transfer.py
@@ -2,32 +2,48 @@ import os
 from flask import Flask, flash, request, redirect, url_for
 from werkzeug.utils import secure_filename
 import datetime
-import lstm
+
+timing = 0
 
 time_stamp = datetime.datetime.now().strftime("%m%d%Y%H%M%S")
-UPLOAD_DIRECTORY = f'/{time_stamp}' 
+UPLOAD_DIRECTORY = f'./{time_stamp}'
+
+f = open("./log")
 
 if not os.path.exists(UPLOAD_DIRECTORY):
     os.makedirs(UPLOAD_DIRECTORY)
 
+if not os.path.exists("./result"):
+    os.makedirs("./result")
+
+if not os.path.exists("./temp"):
+    os.makedirs("./temp")
+
+os.system("rm -rf ./temp/*")
+
 ALLOWED_EXTENSIONS = {'mid'}
 api = Flask(__name__)
 
-# Upload file
-@api.route("/", methods = ['GET','POST'])
-def post_file(filename):
-    if "/" in filename:
-        abort(400, "no subdirectories allowed")
-    with open(os.path.join(UPLOAD_DIRECTORY, filename), "wb") as fp:
-        fp.write(request.data)
-        lstm.main()
-    return "", 201
+# Upload file with name = "example-name"
+@api.route("/post-file", methods = ['POST'])
+def post_file():
+    global timing
+    # post file
+    file = request.files['example-name']    # should be same as name field in <input>
+    filename = secure_filename(file.filename)
+    if file:
+        file.save(os.path.join(UPLOAD_DIRECTORY, filename))
+    # post time
+    timing = request.form['time']           # should be same as name field in <input>
+    return #return webpage or something here after upload
 
-DOWNLOAD_DIRECTORY = f'/result_{time_stamp}'
-if not os.path.exists(DOWNLOAD_DIRECTORY):
-    os.makedirs(DOWNLOAD_DIRECTORY)
+@api.route("/begin-transfer", methods = ['GET','POST'])
+def begin_transfer():
+    os.system(f'python3 ./lstm.py {UPLOAD_DIRECTORY} {timing}')
+    os.system(f'python3 ./predict.py')
+    return
 
 # Download file
-@api.route("/<mid_filename>", methods = ['GET','POST'])
-def get_file(path):
-    return send_from_directory(DOWNLOAD_DIRECTORY, filename = mid_filename, as_attachment=True)
+@api.route("/get-file", methods = ['GET','POST'])
+def get_file():
+    return send_file(f'./result/result_{time_stamp}', attachment_filename = 'result.mid')
