eba8164108afbf983c8dca46fb04b84641fd4b74
Leonard Zhang <54585080+leleonardzhang@users.noreply.github.com>
2020-10-17 20:40:34 -0400

    Add files via upload

--- flask-server/transfer.py
+++ flask-server/transfer.py
@@ -2,32 +2,34 @@ import os
 from flask import Flask, flash, request, redirect, url_for
 from werkzeug.utils import secure_filename
 import datetime
-import lstm
 
 time_stamp = datetime.datetime.now().strftime("%m%d%Y%H%M%S")
-UPLOAD_DIRECTORY = f'/{time_stamp}' 
+UPLOAD_DIRECTORY = f'./{time_stamp}' 
 
 if not os.path.exists(UPLOAD_DIRECTORY):
     os.makedirs(UPLOAD_DIRECTORY)
 
+if not os.path.exists("./result"):
+    os.makedirs("./result")
+
 ALLOWED_EXTENSIONS = {'mid'}
 api = Flask(__name__)
 
-# Upload file
-@api.route("/", methods = ['GET','POST'])
-def post_file(filename):
-    if "/" in filename:
-        abort(400, "no subdirectories allowed")
-    with open(os.path.join(UPLOAD_DIRECTORY, filename), "wb") as fp:
-        fp.write(request.data)
-        lstm.main()
-    return "", 201
+# Upload file with name = "postFile"
+@api.route("/post-file", methods = ['POST'])
+def post_file():
+    file = request.files['']
+    filename = secure_filename(file.filename)
+    if file:
+        file.save(os.path.join(UPLOAD_DIRECTORY, filename))
+    return #return webpage or something here after upload
 
-DOWNLOAD_DIRECTORY = f'/result_{time_stamp}'
-if not os.path.exists(DOWNLOAD_DIRECTORY):
-    os.makedirs(DOWNLOAD_DIRECTORY)
+@api.route("/begin-transfer", methods = ['GET','POST'])
+def begin_transfer():
+    os.system(f'python3 ./lstm.py {UPLOAD_DIRECTORY}')
+    return
 
 # Download file
-@api.route("/<mid_filename>", methods = ['GET','POST'])
-def get_file(path):
-    return send_from_directory(DOWNLOAD_DIRECTORY, filename = mid_filename, as_attachment=True)
+@api.route("/get-file", methods = ['GET','POST'])
+def get_file():
+    return send_file(f'./result/result_{time_stamp}', attachment_filename = 'result.mid')
