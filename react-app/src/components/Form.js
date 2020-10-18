bf5e112ba0bbab74131ec57521a140c4f83e450e
Eric Schneider <u1643364@live.unc.edu>
2020-10-18 01:14:38 -0400

    Front end minus api call done ish

--- react-app/src/components/Form.js
+++ react-app/src/components/Form.js
@@ -11,18 +11,18 @@ export default class Form extends Component {
         this.state = {
             name: "",
             minutes: defaultMinutes,
-            selectedFile: new File(["null"], "null.midi")
+            hasFile: false // This is just to refresh the DOM
         }
 
         this.handleInputChange = this.handleInputChange.bind(this);
-        this.fileInput = null; //React.createRef();
+        this.fileInput = null;
     }
 
     isInputValid(){
         // this is hacky af but works lol
-        let ret = this.state.name.match(/^[\^\-_. \w\d]+$/) &&
+        let ret = this.state.name.match(/^[\^\-_.'"&() \w\d]+$/) &&
             this.state.minutes >= 1 && this.state.minutes <= 200 && this.state.minutes.match(/^\d{1,3}$/) &&
-            this.fileInput;
+            this.fileInput !== null && this.fileInput.type === "audio/mid";
 
         return ret;
     }
@@ -41,61 +41,22 @@ export default class Form extends Component {
                 break;
 
             case "file":
-                /*console.log("hope!")
-                console.log(event.target.files)
-                console.log(event.target.files[0])
-                console.log(target.files[0])
-                console.log("Hope lost!")
-                console.log("" + name + " -> " + target.files[0])
-
-                console.log("Golden test");
-                this.setState({
-                    [name]: target.files[0]
-                });
-                console.log(this.state);
-                this.setState({
-                    selectedFile: target.files[0]
-                });
-                console.log(this.state);
-                this.setState({
-                    "selectedFile": target.files[0]
-                });
-                console.log(this.state);
-                this.setState({
-                    "selectedFile": "eeeee"
-                });
-                console.log(this.state);*/
-
-                console.log("Ticket:")
-                console.log(this.fileInput)
                 this.fileInput = target.files[0];
-                console.log(this.fileInput);
-
-                //console.log(this.state.selectedFile);
-                //console.log(this.state.selectedFile.name);
+                this.setState({
+                    hasFile: true
+                });
 
                 break;
         }
-
-
     }
 
     handleSubmit(event) {
-        // TODO: call api
         event.preventDefault();
         console.log(`Selected file - ${this.fileInput.current.files[0].name}`);
 
         if(this.isInputValid()){ // Want to ignore possible DOM screwups and hacks
-
+            // TODO: call api
         }
-    }
-
-    fileTest(){
-        /*console.log("1")
-        console.log("" + this.fileInput)
-        console.log("2")
-        console.log("" + this.fileInput.current)
-        console.log(`Selected file - ${this.fileInput.current.files[0].name}`);*/
     }
 
     render(){
@@ -134,7 +95,6 @@ export default class Form extends Component {
                                 <input
                                     type="file"
                                     name="selectedFile"
-                                    ref={this.fileInput}
                                     onChange={this.handleInputChange}/>
                             </div>
                         </div>
