9d516a2bf0a4cc2f7f7b30ce9130dd5b64cd4939
Eric Schneider <u1643364@live.unc.edu>
2020-10-18 00:46:17 -0400

    backup before i get rid of some stuff

--- react-app/src/components/Form.js
+++ react-app/src/components/Form.js
@@ -10,34 +10,74 @@ export default class Form extends Component {
 
         this.state = {
             name: "",
-            minutes: defaultMinutes
+            minutes: defaultMinutes,
+            selectedFile: new File(["null"], "null.midi")
         }
 
         this.handleInputChange = this.handleInputChange.bind(this);
-        this.fileInput = React.createRef();
+        this.fileInput = null; //React.createRef();
     }
 
     isInputValid(){
         // this is hacky af but works lol
-        let ret = this.state.name.match(/^[ \w\d]+$/) &&
-            this.state.minutes >= 1 && this.state.minutes <= 200 && this.state.minutes.match(/^\d{1,3}$/);
+        let ret = this.state.name.match(/^[\^\-_. \w\d]+$/) &&
+            this.state.minutes >= 1 && this.state.minutes <= 200 && this.state.minutes.match(/^\d{1,3}$/) &&
+            this.fileInput;
 
-        console.log("Name: " + this.state.name);
         return ret;
     }
 
     handleInputChange(event) {
         const target = event.target;
-        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const value = target.value;
         const name = target.name;
 
-        this.setState({
-            [name]: value
-        });
-        console.log("" + name + " -> " + value);
+        switch(target.type){
+            case "text":
+                this.setState({
+                    [name]: value
+                });
 
-        /*if(this.isInputValid())
-            document.getElementById("subForm");*/
+                break;
+
+            case "file":
+                /*console.log("hope!")
+                console.log(event.target.files)
+                console.log(event.target.files[0])
+                console.log(target.files[0])
+                console.log("Hope lost!")
+                console.log("" + name + " -> " + target.files[0])
+
+                console.log("Golden test");
+                this.setState({
+                    [name]: target.files[0]
+                });
+                console.log(this.state);
+                this.setState({
+                    selectedFile: target.files[0]
+                });
+                console.log(this.state);
+                this.setState({
+                    "selectedFile": target.files[0]
+                });
+                console.log(this.state);
+                this.setState({
+                    "selectedFile": "eeeee"
+                });
+                console.log(this.state);*/
+
+                console.log("Ticket:")
+                console.log(this.fileInput)
+                this.fileInput = target.files[0];
+                console.log(this.fileInput);
+
+                //console.log(this.state.selectedFile);
+                //console.log(this.state.selectedFile.name);
+
+                break;
+        }
+
+
     }
 
     handleSubmit(event) {
@@ -48,6 +88,14 @@ export default class Form extends Component {
         if(this.isInputValid()){ // Want to ignore possible DOM screwups and hacks
 
         }
+    }
+
+    fileTest(){
+        /*console.log("1")
+        console.log("" + this.fileInput)
+        console.log("2")
+        console.log("" + this.fileInput.current)
+        console.log(`Selected file - ${this.fileInput.current.files[0].name}`);*/
     }
 
     render(){
@@ -83,7 +131,11 @@ export default class Form extends Component {
                         <div className="field">
                             <label className="label">Input</label>
                             <div className="file is-primary">
-                                <input type="file" ref={this.fileInput}/>
+                                <input
+                                    type="file"
+                                    name="selectedFile"
+                                    ref={this.fileInput}
+                                    onChange={this.handleInputChange}/>
                             </div>
                         </div>
 
